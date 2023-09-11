58a782afc2b2a6889a0501e19f9351db9ec54776
jaime_westwell <1261360793@qq.com>
2023-09-11 10:55:21 +0800

    cmake_lib

---
+++ cmake_lib/test_xlog/test_xlog.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "xlog.h"
+using namespace std;
+int main()
+{
+	XLog log;
+	cout << "test log " << endl;
+	return 0;
+}
---
+++ cmake_lib/xlog/xlog.h
@@ -0,0 +1,8 @@
+#ifndef XLOG_H
+#define XLOG_H
+class XLog
+{
+public:
+	XLog();
+};
+#endif
