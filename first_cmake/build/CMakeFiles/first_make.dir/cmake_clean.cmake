dd20e7352894fc58dad15eb3fd39e0234480a6d8
jaime_westwell <1261360793@qq.com>
2023-09-11 10:53:08 +0800

    first_cmake

---
+++ first_cmake/build/CMakeFiles/first_make.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_make.dir/first_try.cpp.o"
+  "CMakeFiles/first_make.dir/first_try.cpp.o.d"
+  "first_make"
+  "first_make.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_make.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
